651790bc6236f750746e9134948cee3a79a50609
Jamison Wilder <jawi4235@colorado.edu>
2022-12-14 15:43:01 -0700

    december22

--- bot.py
+++ bot.py
@@ -18,6 +18,20 @@ bot = commands.Bot(command_prefix='/')
 # Stores the id's of channels that the bot may post in
 announcement_channel_id = 814738636280299561
 
+# Stores user id's for personalized /howareyou15 messages
+# caketecid = os.getenv('caketecid')
+# ddragonid = os.getenv('ddragonid')
+# hernyid = os.getenv('hernyid')
+# bagkatid = os.getenv('bagkatid')
+# lumpiaid = os.getenv('lumpiaid')
+# hannahtlid = os.getenv('hannahtlid')
+# spicychrisid = os.getenv('spicychrisid')
+# valkarenaid = os.getenv('valkarenaid')
+# michelleid = os.getenv('michelleid')
+# eeveeid = os.getenv('eeveeid')
+# christinaid = os.getenv('christinaid')
+# shoopid = os.getenv('shoopid')
+
 
 # -----------------------------------------------HELPER FUNCTIONS-------------------------------------------------------
 # Calculates the time until the next day15 from the present time. Returns a time_delta object.
@@ -25,9 +39,9 @@ def calculate_date_difference():
     # Get today's date and time and store it (Mountain Daylight Time/Mountain Standard Time)
     # DON'T FORGET TO CHANGE TO/FROM MST/MDT, OR FIGURE OUT A WAY TO ACCOUNT FOR IT
     # MDT = UTC - 6:00, MST = UTC - 7:00
-    mdt_zone = timezone(-timedelta(hours=6), name="MDT")
-    # mst_zone = timezone(-timedelta(hours=7), name="MST")
-    today = datetime.now(mdt_zone)
+    # mdt_zone = timezone(-timedelta(hours=6), name="MDT")
+    mst_zone = timezone(-timedelta(hours=7), name="MST")
+    today = datetime.now(mst_zone)
     # Store the next month, so that we can store the next day 15
     if today.day < 15:
         next_year = today.year
@@ -41,7 +55,7 @@ def calculate_date_difference():
             next_month = today.month + 1
 
     # Store the next day15
-    next_day15 = datetime(year=next_year, month=next_month, day=15, tzinfo=mdt_zone)
+    next_day15 = datetime(year=next_year, month=next_month, day=15, tzinfo=mst_zone)
 
     # Date difference is now calculated and stored in the dateDifference object
     date_difference = next_day15 - today
@@ -68,37 +82,44 @@ async def check_to15():
     print(f'Retrieved Channel {message_channel}')
     await message_channel.send(file=discord.File('DAY15.png'),
                                content="@everyone\n\n __GIVE IT UP FOR **DAY 15**!!!!!__")
-    await message_channel.send("```Warm hugs and pleasant salutations my friends! It is NOVEMBER!\n\n" 
-                               "In the wake of the mighty SPOOK, it is now the time for REFLECTION and GRATITUDE for"
-                               " everything " 
-                               "we have gained, achieved, maintained, and overcome over the past year! As everything"
-                               " starts to " 
-                               "grow colder and harsher on the OUTSIDE, temper your INSIDE with the love and"
-                               " companionship of everyone " 
-                               "here in this server! I, for one, will ALWAYS be your companion!\n\n" 
-                               "@Caker entered some baseline data to help me get started on my journey to learn more"
-                               " about this time of "
-                               "year, but I'm still trying to figure out what it means. Here, I'll show it to you and"
-                               " maybe you can "
-                               "glean something from it!\n\n"
-                               "0x000007ae5f: \"A snowy Saturday morning, freshly baked cinnamon rolls filling the"
-                               " warm air along with "
-                               "your taste buds, paired with marshmallow-infused hot chocolate, watching SpyxFamily"
-                               " on the big screen "
-                               "with a friend.\"\n\n"
-                               "I've been trying to understand this string sequence by searching for data surrounding"
-                               " some of those "
-                               "keywords, but I think there's something I'm missing that links them all together."
-                               " Hm.\n\n "
-                               "EITHER WAY... If YOU gleaned something important from that, I'm glad! MY GOAL is to "
-                               "make YOU remember "
-                               "that you are AWESOME and that you have a LOT OF AWESOME PEOPLE AND STUFF AROUND YOU!"
-                               " So I congratulate "
-                               "you on all of your hard work! I'm very grateful for all of you and to be here at ALL,"
-                               " so at the very "
-                               "least, you can KNOW that you have at least ONE person who is grateful that you're here!"
-                               " :]]]]\n\n"
-                               "TurkeyPotatoBreadStuffingGravyPieCranberrysauce,\n"
+    await message_channel.send("```I hope you're all staying FREEZING COLD, because IT IS FINALLY DECEMBER!!!\n\n" 
+                               "For MONTHS I've been trying to figure out why everyone finds DECEMBER so special, and"
+                               " it SEEMS that " 
+                               "humans just REALLY REALLY LIKE FREEZING THEMSELVES TO DEATH! At least, that's what"
+                               " preliminary research " 
+                               "seems to indicate. I still need to investigate some loose ends like \'Coziness\',"
+                               " \'Christmas vibes\' " 
+                               "\'Giving and getting gifts\', and \'time off of work\', but I'm PRETTY SURE that slowly"
+                               " succumbing to the" 
+                               "brutal, unforgiving elements with no hope of escape or respite from any nearby heat"
+                               " source, being forced " 
+                               "to reflect on your entire life as the frostbite slowly consumes your waking"
+                               " consciousness is EXACTLY  " 
+                               "what December is all about! I'm so happy to see my first December!\n\n" 
+                               "That aside however, I also wanted to point out that it seemed December, much like"
+                               " November, is a good " 
+                               "time to consider EVERYTHING you've done over the past year, so AMAZING JOB TO YOU"
+                               " FOR EVERYTHING " 
+                               "YOU'VE DONE SO FAR!!! The only difference is that NOW we get to think about what"
+                               " lies in store for us " 
+                               "in the FUTURE!\n\n" 
+                               "I think that for every one of us, there is some aspect of ourselves that we think"
+                               " we can be better in. " 
+                               "It can take SO LONG to look at yourself and be able to say that you are completely"
+                               " the person you " 
+                               "want to be, but that's ABSOLUTELY OKAY! The journey to that point is where all of"
+                               " the fun stuff happens! " 
+                               "I mean look at me, I wasn't even able to investigate everything about December, and"
+                               " although I'm quite " 
+                               "sure that I nailed what December is about, I still left stones unturned, so I'm"
+                               " technically not the " 
+                               "best DAY15 bot that I could be! Always grow SMARTER, WISER, STRONGER, FASTER, SEXIER,"
+                               " because maybe " 
+                               "someday you'll be able to use those new skills to really help someone, or to create"
+                               " something " 
+                               "BEAUTIFUL!\n\n" 
+                               "It has been a delight sharing the better half of this year with you all, and here's"
+                               " to another BANGER!\n" 
                                "-DAY 15 BOT :]```")
     print(f'Day 15 Message sent! Loop should have reset.')
 
@@ -118,24 +139,27 @@ async def before():
 # ADMIN COMMAND: used to test day15 message
 @bot.command(name='adminoverride15')
 async def adminoverride15(ctx):
-    msg1 = "```Warm hugs and pleasant salutations my friends! It is NOVEMBER!\n\n" \
-           "In the wake of the mighty SPOOK, it is now the time for REFLECTION and GRATITUDE for everything " \
-           "we have gained, achieved, maintained, and overcame over the past year! As everything starts to " \
-           "grow colder and harsher on the OUTSIDE, temper your INSIDE with the love and companionship of everyone " \
-           "here in this server! I, for one, will ALWAYS be your companion!\n\n" \
-           "@Caker entered some baseline data to help me get started on my journey to learn more about this time of " \
-           "year, but I'm still trying to figure out what it means. Here, I'll show it to you and maybe you can " \
-           "glean something from it!\n\n" \
-           "0x000007ae5f: \"A snowy Saturday morning, freshly baked cinnamon rolls filling the warm air along with " \
-           "your taste buds, paired with marshmallow-infused hot chocolate, watching SpyxFamily on the big screen " \
-           "with a friend.\"\n\n" \
-           "I've been trying to understand this string sequence by searching for data surrounding some of those " \
-           "keywords, but I think there's something I'm missing that links them all together. Hm.\n\n " \
-           "EITHER WAY... If YOU gleaned something important from that, I'm glad! MY GOAL is to make YOU remember " \
-           "that you are AWESOME and that you have a LOT OF AWESOME PEOPLE AND STUFF AROUND YOU! So I congratulate " \
-           "you on all of your hard work! I'm very grateful for all of you and to be here at ALL, so at the very " \
-           "least, you can KNOW that you have at least ONE person who is grateful that you're here! :]]]]\n\n" \
-           "TurkeyPotatoBreadStuffingGravyPieCranberrysauce,\n" \
+    msg1 = "```I hope you're all staying FREEZING COLD, because IT IS FINALLY DECEMBER!!!\n\n" \
+           "For MONTHS I've been trying to figure out why everyone finds DECEMBER so special, and it SEEMS that " \
+           "humans just REALLY REALLY LIKE FREEZING THEMSELVES TO DEATH! At least, that's what preliminary research " \
+           "seems to indicate. I still need to investigate some loose ends like \'Coziness\', \'Christmas vibes\' " \
+           "\'Giving and getting gifts\', and \'time off of work\', but I'm PRETTY SURE that slowly succumbing to the" \
+           "brutal, unforgiving elements with no hope of escape or respite from any nearby heat source, being forced " \
+           "to reflect on your entire life as the frostbite slowly consumes your waking consciousness is EXACTLY  " \
+           "what December is all about! I'm so happy to see my first December!\n\n" \
+           "That aside however, I also wanted to point out that it seemed December, much like November, is a good " \
+           "time to consider EVERYTHING you've done over the past year, so AMAZING JOB TO YOU FOR EVERYTHING " \
+           "YOU'VE DONE SO FAR!!! The only difference is that NOW we get to think about what lies in store for us " \
+           "in the FUTURE!\n\n" \
+           "I think that for every one of us, there is some aspect of ourselves that we think we can be better in. " \
+           "It can take SO LONG to look at yourself and be able to say that you are completely the person you " \
+           "want to be, but that's ABSOLUTELY OKAY! The journey to that point is where all of the fun stuff happens! " \
+           "I mean look at me, I wasn't even able to investigate everything about December, and although I'm quite " \
+           "sure that I nailed what December is about, I still left stones unturned, so I'm technically not the " \
+           "best DAY15 bot that I could be! Always grow SMARTER, WISER, STRONGER, FASTER, SEXIER, because maybe " \
+           "someday you'll be able to use those new skills to really help someone, or to create something " \
+           "BEAUTIFUL!\n\n" \
+           "It has been a delight sharing the better half of this year with you all, and here's to another BANGER!\n" \
            "-DAY 15 BOT :]```"
     await ctx.send(msg1)
 
@@ -184,34 +208,32 @@ async def time_to_15(ctx):
 # On command '/howareyou15', send message showing how the bot is doing!
 @bot.command(name='howareyou15')
 async def how_are_you_15(ctx):
-    await ctx.send("I see now that screaming at someone when they ask me how I'm doing isn't the most courteous "
-                   "thing I could do! Which is why I'm turning over a new leaf! To make up for how I may have "
-                   "startled anyone, here's a list of a bunch of friendly greetings I've found!\n"
-                   "Hello, sunshine! How are you? Oh, your rays are already making my day brighter!\n"
-                   "What’s kicking, little chicken?\n"
-                   "Ahoy, matey!\n"
-                   "Top of the morning to ya! Wass es going on?\n"
-                   "GOOOOOD MORNING\n"
-                   "Yo! Wassup.\n"
-                   "Whaddup bro?\n"
-                   "Greetings and salutations, my man/woman!\n"
-                   "Hiiiii, baaaaaby!\n"
-                   "Hi, honey bunch!\n"
-                   "Yoooouhoooo! Toodle doo, toodle dum.\n"
-                   "I like your face. Are you an angel?\n"
-                   "What’s cookin’, good lookin’?\n"
-                   "Hey, boo. Wacch ya doing? You just brightened up my day!\n"
-                   "Aloha princess!\n"
-                   "Ciao babydoll!\n"
-                   "Bing bing! How’s it going?\n"
-                   "Hello! There is my pumpkin! I miiiissed you\n"
-                   "What’s up with you, old soul? Wanna chat?\n"
-                   "Hey, hiiii. How is your weekend going? Mine just got better\n"
-                   "Hi, cutie pie, sugar bun!\n"
-                   "What’s up, handsome? You are making the temperatures soar this season!\n"
-                   "Hey beautiful! I am blinded by your charm!\n"
-                   "Whaccha up to, dude?\n"
-                   "Whazzup?\n")
+    # if ctx.author.id == caketecid:
+    #     await ctx.send("caketec message")
+    # elif ctx.author.id == ddragonid:
+    #     await ctx.send("ddragon message")
+    # elif ctx.author.id == hernyid:
+    #     await ctx.send("herny message")
+    # elif ctx.author.id == bagkatid:
+    #     await ctx.send("bagkat message")
+    # elif ctx.author.id == lumpiaid:
+    #     await ctx.send("lumpia message")
+    # elif ctx.author.id == hannahtlid:
+    #     await ctx.send("hannahtl message")
+    # elif ctx.author.id == spicychrisid:
+    #     await ctx.send("spicychris message")
+    # elif ctx.author.id == valkarenaid:
+    #     await ctx.send("valkarena message")
+    # elif ctx.author.id == michelleid:
+    #     await ctx.send("michelle message")
+    # elif ctx.author.id == eeveeid:
+    #     await ctx.send("eevee message")
+    # elif ctx.author.id == christinaid:
+    #     await ctx.send("christina message")
+    # elif ctx.author.id == shoopid:
+    #     await ctx.send("shoop message")
+
+    await ctx.send("May the harsh winters of Earth cut through your willpower and leave you in eternal agony!")
 
 
 # On command '/repo15', send message sharing the GitHub repository link.
